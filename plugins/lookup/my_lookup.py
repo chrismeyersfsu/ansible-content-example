b1f52fa19681f6a3a84f602b2c6f517b1f7bba3f
chris meyers <chris.meyers.fsu@gmail.com>
2019-05-10 14:05:15 -0400

    help galaxy import

--- plugins/lookup/my_lookup.py
+++ plugins/lookup/my_lookup.py
@@ -1,5 +1,13 @@
 from ansible.plugins.lookup import LookupBase
 
+DOCUMENTATION = """
+    lookup: my_lookup
+    author: Chris Meyers
+    short_description: it is a lookup
+    description:
+        - must have this otherwise galaxy will fail to import
+"""
+
 class LookupModule(LookupBase):
     def run(self, terms, variables, **kwargs):
         return ['lookup_from_user_dir']
